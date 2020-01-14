c38408aa1e4c4d88e7bb7eb791e9218453a3724d
jenny <jenny998chen@gmail.com>
2020-01-14 14:42:04 -0500

    socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import socketIOClient from "socket.io-client";
+let socket = socketIOClient("localhost:3001");
 function App() {
   useEffect(() => {
+    socket.emit("test")
+    socket.on("test", data => console.log(data));
     fetch("/login")
         .then(res => res.text())
         .then(res => console.log(res));
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3001;
-
+io.origins('*:*')
 // app.use(express.static('public'));
 var path = require('path');                    
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -14,4 +14,54 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/login', (req, res) => {
     res.json({data:'heoll'})
 })
-app.listen(port);
+var users = [];
+var chatMsg=[
+];
+io.on('connection', socket => {
+    // console.log(io.sockets.sockets);
+    socket.on('test', m => {
+        io.emit("test","hi")
+    });
+    socket.on('join room', function(newroom){
+      if(socket.room){
+        socket.leave(socket.room);
+      }
+      // join new room, received as function parameter
+      // console.log(socket.room,socket.rooms)
+      socket.join(newroom);
+      socket.room = newroom;
+  
+  
+      io.in(newroom).clients((err , clients) => {
+        console.log(clients)
+      });
+      // io.emit('messssageage', "this is a test"); //sending to all clients, include sender
+      // io.in('game').emit('message', 'cool game'); 
+      // io.sockets.in('room1').emit('function', {foo:bar});
+      console.log(socket.room,socket.rooms)
+      socket.broadcast.to(newroom).emit('updatechat', socket.id+' joined');
+      // socket.room = newroom;
+      
+    });
+    socket.on('chat message', function(msg){
+      chatMsg.push(msg);
+      socket.broadcast.emit('chat message', msg);
+    });
+    socket.on('add user', name => {
+      socket.emit('prev',{users,'chats':chatMsg});
+      socket.username = name;
+      users.push(name);
+      socket.broadcast.emit('user joined', name);
+    });
+    socket.on('typing', data => {
+      socket.broadcast.emit('typing', {
+        typing: data,
+        username: socket.username
+      });
+    });
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user left', socket.username);
+      users=users.filter(i => i !== socket.username);
+    });
+  });
+  server.listen(port);
